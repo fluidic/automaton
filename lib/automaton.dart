e4223affd57bbaa2891c0217014876c43f74b4cc
Kyusun Kim <maniagoon@gmail.com>
2016-06-30 16:57:46 +0900

    Arrange import order in automaton.dart

--- lib/automaton.dart
+++ lib/automaton.dart
@@ -1,8 +1,8 @@
 library automaton;
 
 export 'src/automaton.dart';
-export 'src/state.dart';
-export 'src/transition.dart';
 export 'src/basic_automata.dart';
 export 'src/basic_operations.dart';
 export 'src/minimization_operations.dart';
+export 'src/state.dart';
+export 'src/transition.dart';
